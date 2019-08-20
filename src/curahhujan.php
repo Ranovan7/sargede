82360cb184fdbf6327bc0bf96eb8e82d46fe5f12
romyranovan <romyranovan@gmail.com>
2019-08-20 11:00:16 +0700

    add more urls for tma and curahhujan, add templates for those urls

--- src/curahhujan.php
+++ src/curahhujan.php
@@ -10,4 +10,32 @@ $app->group('/curahhujan', function() {
     $this->get('[/]', function(Request $request, Response $response, $args) {
         return $this->view->render($response, 'curahhujan/index.html');
     });
+
+    $this->group('/{id}', function() {
+
+        $this->get('[/]', function(Request $request, Response $response, $args) {
+            return $this->view->render($response, 'curahhujan/jamjaman.html');
+        });
+    });
+
+    $this->group('/{id}/harian', function() {
+
+        $this->get('[/]', function(Request $request, Response $response, $args) {
+            return $this->view->render($response, 'curahhujan/harian.html');
+        });
+    });
+
+    $this->group('/{id}/bulanan', function() {
+
+        $this->get('[/]', function(Request $request, Response $response, $args) {
+            return $this->view->render($response, 'curahhujan/bulanan.html');
+        });
+    });
+
+    $this->group('/{id}/maksimum', function() {
+
+        $this->get('[/]', function(Request $request, Response $response, $args) {
+            return $this->view->render($response, 'curahhujan/maksimum.html');
+        });
+    });
 });
--- src/tma.php
+++ src/tma.php
@@ -10,4 +10,11 @@ $app->group('/tma', function() {
     $this->get('[/]', function(Request $request, Response $response, $args) {
         return $this->view->render($response, 'tma/index.html');
     });
+
+    $this->group('/{id}', function() {
+
+        $this->get('[/]', function(Request $request, Response $response, $args) {
+            return $this->view->render($response, 'tma/show.html');
+        });
+    });
 });
