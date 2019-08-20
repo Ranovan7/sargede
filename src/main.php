43cf276a8e6e64baae5e48287701ee38f26c36f2
romyranovan <romyranovan@gmail.com>
2019-08-20 14:19:46 +0700

    checking twig templating

--- src/main.php
+++ src/main.php
@@ -8,7 +8,55 @@ use Slim\Http\Response;
 // home
 
 $app->get('[/]', function(Request $request, Response $response, $args) {
-    return $this->view->render($response, 'main/index.html');
+    return $this->view->render($response, 'main/index.html', [
+        'hujan_sejak' => 90,
+        'list_tma' => [
+            [
+                'waktu' => "19 Agustus 2019",
+                'jam' => "16:00",
+                'pos' => "Pos Air 21",
+                'wlev' => 58
+            ],
+            [
+                'waktu' => "19 Agustus 2019",
+                'jam' => "17:00",
+                'pos' => "Pos Air 22",
+                'wlev' => 100
+            ],
+            [
+                'waktu' => "19 Agustus 2019",
+                'jam' => "17:00",
+                'pos' => "Pos Air 23",
+                'wlev' => 30
+            ],
+        ],
+        'list_hujan' => [
+            [
+                'waktu' => "19 Agustus 2019",
+                'daftar' => [
+                    [
+                        'pos' => "Pos Air 1",
+                        'ch' => 18.0,
+                        'durasi' => 55
+                    ],
+                ]
+            ],
+            [
+                'waktu' => "18 Agustus 2019",
+                'daftar' => [
+                    [
+                        'pos' => "Pos Air 1",
+                        'ch' => 10.0,
+                        'durasi' => 30
+                    ],[
+                        'pos' => "Pos Air 2",
+                        'ch' => 8.0,
+                        'durasi' => 25
+                    ]
+                ]
+            ]
+        ]
+    ]);
 });
 
 $app->get('/dashboard', function(Request $request, Response $response, $args) {
