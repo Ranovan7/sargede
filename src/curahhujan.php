0b443bfc66e32030eafb97cc65e737e92e71674e
romyranovan <romyranovan@gmail.com>
2019-08-21 21:55:26 +0700

    fix periodik data display on curahhujan index, fix routing in curahhujan index, fix sampling routing in curahhujan index and jamjaman

--- src/curahhujan.php
+++ src/curahhujan.php
@@ -8,45 +8,104 @@ use Slim\Http\Response;
 $app->group('/curahhujan', function() {
 
     $this->get('[/]', function(Request $request, Response $response, $args) {
+        $hari = $request->getParam('sampling', "2019-06-26");//date('Y-m-d');
+        $prev_date = date('Y-m-d', strtotime($hari .' -1day'));
+        $next_date = date('Y-m-d', strtotime($hari .' +1day'));
+
+        $end = date('Y-m-d', strtotime($hari .' +1day'));
+        $from = "{$hari} 07:00:00";
+        $to = "{$end} 06:55:00";
+
+        $lokasi = $this->db->query("SELECT * FROM lokasi WHERE lokasi.jenis='1' OR lokasi.jenis='4'")->fetchAll();
+
+        $result = [];
+        foreach ($lokasi as $l) {
+            $ch = $this->db->query("SELECT * FROM periodik
+                                    WHERE lokasi_id = {$l['id']} AND rain IS NOT NULL
+                                        AND sampling BETWEEN '{$from}' AND '{$to}'
+                                    ORDER BY sampling")->fetchAll();
+
+            $durasi_07_13 = 0;
+            $durasi_13_19 = 0;
+            $durasi_19_01 = 0;
+            $durasi_01_07 = 0;
+            $durasi_all = 0;
+
+            foreach ($ch as $c) {
+                $time = date('H:i:s', strtotime(date('H:i:s', strtotime($c['sampling'])) .' -7hour'));
+                if ($time < '07:00:00') {
+                    $durasi_07_13 += $c['rain'];
+                } else if ($time < '13:00:00') {
+                    $durasi_13_19 += $c['rain'];
+                } else if ($time < '19:00:00') {
+                    $durasi_19_01 += $c['rain'];
+                } else {
+                    $durasi_01_07 += $c['rain'];
+                }
+                $durasi_all += $c['rain'];
+            };
+
+            $result[] = [
+                'lokasi' => $l,
+                'durasi_07_13' => $durasi_07_13,
+                'durasi_13_19' => $durasi_13_19,
+                'durasi_19_01' => $durasi_19_01,
+                'durasi_01_07' => $durasi_01_07,
+                'durasi_all' => $durasi_all
+            ];
+        }
+
         return $this->view->render($response, 'curahhujan/index.html', [
-            "sampling" => "19 Agustus 2019",
-            "lokasi" => [
-                [
-                    'id' => 1,
-                    'nama' => "Pos Air 1"
-                ],
-                [
-                    'id' => 2,
-                    'nama' => "Pos Air 2"
-                ]
-            ],
-            "result" => [
-                [
-                    'durasi_07-13' => 15.0,
-                    'durasi_13-19' => 10.0,
-                    'durasi_19-01' => 5.0,
-                    'durasi_01-07' => 20.0,
-                    'durasi_all' => 50.0
-                ],
-                [
-                    'durasi_07-13' => 15.0,
-                    'durasi_13-19' => 10.0,
-                    'durasi_19-01' => 5.0,
-                    'durasi_01-07' => 20.0,
-                    'durasi_all' => 50.0
-                ]
-            ]
+            'sampling' => tanggal_format(strtotime($hari)),
+            'prev_date' => $prev_date,
+            'next_date' => $next_date,
+            'result' => $result
         ]);
     })->setName('curahhujan');
 
     $this->group('/{id}', function() {
 
         $this->get('[/]', function(Request $request, Response $response, $args) {
+            $hari = $request->getParam('sampling', "2019-06-26");//date('Y-m-d');
+            $prev_date = date('Y-m-d', strtotime($hari .' -1day'));
+            $next_date = date('Y-m-d', strtotime($hari .' +1day'));
+
+            $end = date('Y-m-d', strtotime($hari .' +1day'));
+            $from = "{$hari} 07:00:00";
+            $to = "{$end} 06:55:00";
+
+            $lokasi_id = $request->getAttribute('id');
+            $lokasi = $this->db->query("SELECT * FROM lokasi WHERE id={$lokasi_id}")->fetch();
+
+            $ch = $this->db->query("SELECT * FROM periodik
+                                    WHERE lokasi_id = {$lokasi_id} AND rain IS NOT NULL
+                                        AND sampling BETWEEN '{$from}' AND '{$to}'
+                                    ORDER BY sampling")->fetchAll();
+            $result = [
+                'datasets' => [],
+                'labels' => []
+            ];
+            $check = 6;
+            $i = 0;
+            foreach ($ch as $c) {
+                $current = date('H', $c['sampling']);
+                if ($current != $check) {
+                    $check = $current;
+                    $i += 1;
+                    $result['datasets'][] = 0;
+                    $result['labels'][] = date('Y-m-d H', strtotime($c['sampling']));;
+                };
+
+                $result['datasets'][$i] += $c['rain'];
+            };
+            // echo implode(",", $result['datasets']);
+
             return $this->view->render($response, 'curahhujan/jamjaman.html', [
-                'pos' => [
-                    'id' => 1,
-                    'nama' => "Pos Air 1"
-                ]
+                'sampling' => tanggal_format(strtotime($hari)),
+                'lokasi' => $lokasi,
+                'prev_date' => $prev_date,
+                'next_date' => $next_date,
+                'result' => $result
             ]);
         })->setName('curahhujan.jamjaman');
     });
