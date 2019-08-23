c4e69ea6b370c9d542bab1fbe1096c0ec2007f13
romyranovan <romyranovan@gmail.com>
2019-08-23 21:44:28 +0700

    fix show latest info about percipitation on main index

--- src/main.php
+++ src/main.php
@@ -8,54 +8,79 @@ use Slim\Http\Response;
 // home
 
 $app->get('[/]', function(Request $request, Response $response, $args) {
-    return $this->view->render($response, 'main/index.html', [
-        'hujan_sejak' => 90,
-        'list_tma' => [
-            [
-                'waktu' => "19 Agustus 2019",
-                'jam' => "16:00",
-                'pos' => "Pos Air 21",
-                'wlev' => 58
-            ],
-            [
-                'waktu' => "19 Agustus 2019",
-                'jam' => "17:00",
-                'pos' => "Pos Air 22",
-                'wlev' => 100
-            ],
-            [
-                'waktu' => "19 Agustus 2019",
-                'jam' => "17:00",
-                'pos' => "Pos Air 23",
-                'wlev' => 30
-            ],
+    $sejak = intval($request->getParam('sejak', 90));
+    $end = date("Y-m-d");
+    $start = date('Y-m-d', strtotime($end ." -{$sejak}day"));
+    $from = "{$start} 07:00:00";
+    $to = "{$end} 06:55:00";
+
+    // get curahhujan list for "sejak" day
+    $ch = $this->db->query("SELECT periodik.*, lokasi.nama AS lokasi_nama FROM periodik
+                            LEFT JOIN lokasi ON periodik.lokasi_id=lokasi.id
+                            WHERE periodik.rain IS NOT NULL
+                                AND periodik.sampling BETWEEN '{$from}' AND '{$to}'
+                            ORDER BY periodik.sampling DESC")->fetchAll();
+
+    $tma = [
+        [
+            'waktu' => "19 Agustus 2019",
+            'jam' => "16:00",
+            'pos' => "Pos Air 21",
+            'wlev' => 58
         ],
-        'list_hujan' => [
-            [
-                'waktu' => "19 Agustus 2019",
-                'daftar' => [
-                    [
-                        'pos' => "Pos Air 1",
-                        'ch' => 18.0,
-                        'durasi' => 55
-                    ],
-                ]
-            ],
-            [
-                'waktu' => "18 Agustus 2019",
-                'daftar' => [
-                    [
-                        'pos' => "Pos Air 1",
-                        'ch' => 10.0,
-                        'durasi' => 30
-                    ],[
-                        'pos' => "Pos Air 2",
-                        'ch' => 8.0,
-                        'durasi' => 25
-                    ]
-                ]
-            ]
-        ]
+        [
+            'waktu' => "19 Agustus 2019",
+            'jam' => "17:00",
+            'pos' => "Pos Air 22",
+            'wlev' => 100
+        ],
+        [
+            'waktu' => "19 Agustus 2019",
+            'jam' => "17:00",
+            'pos' => "Pos Air 23",
+            'wlev' => 30
+        ],
+    ];
+
+    $result = [
+        'tma' => [],
+        'curahhujan' => []
+    ];
+    $result['tma'] = $tma;
+
+    $current_date = Null;
+    foreach ($ch as $c) {
+        // check sampling (date) change every iteration to append new array
+        $date = date('Y-m-d', strtotime($c['sampling'].' -7hour'));
+        if ($date != $current_date) {
+            $result['curahhujan'][$date] = [
+                'waktu' => tanggal_format(strtotime($date)),
+                'date' => $date,
+                'daftar' => []
+            ];
+            $current_date = $date;
+        }
+
+        // check lokasi id to add
+        $lokasi = $c['lokasi_nama'];
+        if (array_key_exists($c['lokasi_id'], $result['curahhujan'][$date]['daftar'])) {
+            // update ch and durasi
+            $result['curahhujan'][$date]['daftar'][$c['lokasi_id']]['ch'] = round($result['curahhujan'][$date]['daftar'][$c['lokasi_id']]['ch'] + $c['rain'], 2);
+            $result['curahhujan'][$date]['daftar'][$c['lokasi_id']]['durasi'] += 5;
+        } else {
+            // append new array cause its not exist
+            $result['curahhujan'][$date]['daftar'][$c['lokasi_id']] = [
+                'id' => $c['lokasi_id'],
+                'lokasi' => $lokasi,
+                'ch' => $c['rain'],
+                'durasi' => 5
+            ];
+        }
+    }
+
+    return $this->view->render($response, 'main/index.html', [
+        'hujan_sejak' => $sejak,
+        'result' => $result,
     ]);
 });
 
