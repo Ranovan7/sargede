0fc7dd7d18f4ae54c5319ac98cf290ee9f5d87c4
rpahlevy <rio.pahlevy@gmail.com>
2019-08-24 01:17:28 +0700

    NAVBAR: - fix bug BG navbar atas transparan
    TMA
    - show detail & seleksi range dari sampling

--- src/tma.php
+++ src/tma.php
@@ -17,6 +17,7 @@ $app->group('/tma', function() {
         $lokasi = $this->db->query("SELECT * FROM lokasi WHERE lokasi.jenis='2'")->fetchAll();
         $result = [];
         foreach ($lokasi as $l) {
+            // LOGGER
             $wlev = $this->db->query("SELECT * FROM periodik
                                     WHERE lokasi_id = {$l['id']} AND wlev IS NOT NULL
                                         AND sampling BETWEEN '{$from}' AND '{$to}'
@@ -59,6 +60,37 @@ $app->group('/tma', function() {
             if (!empty($latest_time)) {
                 $latest_time = date('H:i', strtotime($latest_time));
             }
+
+
+            // MANUAL
+            $wlev_manual = $this->db->query("SELECT * FROM tma
+                                    WHERE lokasi_id = {$l['id']} AND telemetri IS NOT NULL
+                                        AND sampling BETWEEN '{$from}' AND '{$to}'
+                                    ORDER BY sampling")->fetchAll();
+
+            $jam6_manual = 0;
+            $jam12_manual = 0;
+            $jam18_manual = 0;
+            
+            foreach ($wlev_manual as $w) {
+                $time = date('H:i', strtotime($w['sampling']));
+                switch ($time) {
+                    case '06:00':
+                        $jam6_manual = $w['telemetri'];
+                        break;
+                    case '12:00':
+                        $jam12_manual = $w['telemetri'];
+                        break;
+                    case '18:00':
+                        $jam18_manual = $w['telemetri'];
+                        break;
+                }
+            }
+            
+            $jam6_manual = $jam6_manual > 0 ? number_format($jam6_manual,1) : '-';
+            $jam12_manual = $jam12_manual > 0 ? number_format($jam12_manual,1) : '-';
+            $jam18_manual = $jam18_manual > 0 ? number_format($jam18_manual,1) : '-';
+
             
             $result[] = [
                 'lokasi' => $l,
@@ -68,6 +100,9 @@ $app->group('/tma', function() {
                 'jam0' => $jam0,
                 'latest_wlev' => $latest_wlev,
                 'latest_time' => $latest_time,
+                'jam6_manual' => $jam6_manual,
+                'jam12_manual' => $jam12_manual,
+                'jam18_manual' => $jam18_manual,
             ];
         }
         
@@ -83,21 +118,57 @@ $app->group('/tma', function() {
 
         $this->get('[/]', function(Request $request, Response $response, $args) {
             $lokasi = $request->getAttribute('lokasi');
-            $now = "2019-06-26";//date('Y-m-d');
-            $start_date = $request->getParam('start_date', $now);
-            $end_date = $request->getParam('end_date', date('Y-m-d', strtotime('-2day')));
+            $now = "2019-06-28";//date('Y-m-d');
+            $start_date = $request->getParam('start_date', date('Y-m-d', strtotime("$now -2day")));
+            $end_date = $request->getParam('end_date', $now);
 
-            $result = [];
-            $datasets = [];
-            $labels = [];
+            // preparing initial datasets (0s) and labels (hour)
+            $result = [
+                'datasets' => [],
+                'labels' => []
+            ];
+
+            $from = "{$start_date} 00:00:00";
+            $to = "{$end_date} 23:55:00";
+            $wlev = $this->db->query("SELECT * FROM periodik
+                                    WHERE lokasi_id = {$lokasi['id']} AND wlev IS NOT NULL
+                                        AND sampling BETWEEN '{$from}' AND '{$to}'
+                                    ORDER BY sampling")->fetchAll();
+
+            $prev_time = strtotime($from);
+            $data = [];
+            foreach ($wlev as $w) {
+                $current_time = strtotime($w['sampling']);
+                while ($prev_time <= $current_time) {
+                    $prev_tanggal = tanggal_format($prev_time, true);
+                    $result['labels'][] = $prev_tanggal;
+                    $data[] = $prev_time == $current_time ? number_format($w['wlev'],2) : 0;
+                    $prev_time += 300;
+                }
+            }
+
+            while ($prev_time <= strtotime($to)) {
+                $prev_tanggal = tanggal_format($prev_time, true);
+                $result['labels'][] = $prev_tanggal;
+                $data[] = 0;
+                $prev_time += 300;
+            }
+            
+            $result['datasets'][] = [
+                'label' => "Tinggi Mata Air",
+                'data' => $data,
+                'backgroundColor' => "rgba(255, 255, 255, 0.5)",
+                'borderColor' => "rgba(205, 50, 0, 0.5)",
+                'fill' => false
+            ];
+
+            // dump($result);
 
             return $this->view->render($response, 'tma/show.html', [
-                'start_date' => tanggal_format(strtotime($start_date)),
-                'end_date' => tanggal_format(strtotime($end_date)),
+                'start_date' => $start_date,
+                'end_date' => $end_date,
                 'lokasi' => $lokasi,
                 'result' => $result,
-                'datasets' => $datasets,
-                'labels' => $labels,
             ]);
         })->setName('tma.show');
 
