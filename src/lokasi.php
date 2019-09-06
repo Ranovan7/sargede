4daf3c2388ed0628970a3999fb1224000fa685a4
Ranovan7 <romyranovan@gmail.com>
2019-09-06 16:28:31 +0700

    /lokasi authorization, check flash msg, FE fix
    - limit access to '/lokasi' only to admin, if not admin redirect to /login
    - add flash messages to '/' and '/lokasi' (just checking)
    - no changes in flash.html just somehow still modified after undo

--- src/lokasi.php
+++ src/lokasi.php
@@ -14,4 +14,30 @@ $app->group('/lokasi', function() {
         ]);
     })->setName('lokasi');
 
+})->add(function(Request $request, Response $response, $next) {
+
+    $user_refresh_time = $this->session->user_refresh_time;
+    $now = time();
+
+    // cek masa aktif login
+    if (empty($user_refresh_time) || $user_refresh_time < $now) {
+        $this->session->destroy();
+        // die('Silahkan login untuk melanjutkan');
+        return $this->response->withRedirect('/login');
+    }
+
+    // cek user exists, ada di index.php
+    $user = $this->user;
+    if (!$user) {
+        throw new \Slim\Exception\NotFoundException($request, $response);
+    }
+    if ($user['role'] != "1") {
+        $this->flash->addMessage('errors', 'Hanya admin yang diperbolehkan mengakses laman tersebut.');
+        return $this->response->withRedirect('/login');
+    }
+
+    // inject user ke dalam request agar bisa diakses di route
+    $request = $request->withAttribute('user', $user);
+
+    return $next($request, $response);
 });
